a6048eb3906cb53c22465f3c5f8b40e0e53cadc6
João Victor Scanagatta <joaoscanagatta1@hotmail.com>
2017-10-26 21:35:55 -0200

    3 tipos de relatorios agencia

--- RevisaoOO/src/application/AgenciaController.java
+++ RevisaoOO/src/application/AgenciaController.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import br.edu.unoesc.revisaoOO.modelo.Agencia;
@@ -19,10 +21,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.view.JasperViewer;
 
 public class AgenciaController {
@@ -77,13 +81,72 @@ public class AgenciaController {
 	private Button btnRelatorio;
 
 	@FXML
+	private Button btnRelatorio1;
+
+	@FXML
+	void onRelatorioAgencia(ActionEvent event) {
+		URL url = getClass().getResource("/RelatorioAgencia.jasper");
+
+		try {
+
+			Map<String, Object> parametros = new HashMap<>(); // passa parametros
+			parametros.put("nomeAgencia", "%sa%");
+			
+			JasperPrint print = JasperFillManager.fillReport(url.getPath(), null, ConexaoUtil.getCon());
+			// JasperPrint print =
+			// JasperFillManager.fillReport("c:/RelatorioUF.jasper", parametros,
+			// ConexaoUtil.getCon());
+
+			JasperViewer.viewReport(print);
+			JasperExportManager.exportReportToPdfFile(print, "relatorioAgencia.pdf");
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	private Button btnRelatorio2;
+
+	@FXML
+	void onRelatorioAgenciaObjeto(ActionEvent event) {
+		URL url = getClass().getResource("/RelatorioAgenciaObjeto.jasper");
+
+		try {
+
+			// aqui ele pega dos objetos e da classe AgenciaDao
+			Map<String, Object> parametros = new HashMap<>(); // passa parametros
+			parametros.put("nomeAgencia", "%sa%");
+			
+			JRDataSource dataSource = new JRBeanCollectionDataSource(agenciaDao.listar());
+			
+			JasperPrint print = JasperFillManager.fillReport(url.getPath(), null, dataSource);
+			// JasperPrint print =
+			// JasperFillManager.fillReport("c:/RelatorioUF.jasper", parametros,
+			// ConexaoUtil.getCon());
+
+			JasperViewer.viewReport(print);
+			JasperExportManager.exportReportToPdfFile(print, "relatorioAgencia.pdf");
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
 	void onReports(ActionEvent event) {
 
 		URL url = getClass().getResource("/RelatorioUF.jasper");
 
 		try {
-			//JasperPrint print = JasperFillManager.fillReport(url.getPath(), null, ConexaoUtil.getCon());
-			JasperPrint print = JasperFillManager.fillReport("c:/RelatorioUF.jasper", null, ConexaoUtil.getCon());
+
+			Map<String, Object> parametros = new HashMap<>(); // passa
+																// paremetros
+																// pra dentro do
+																// relatorio
+			parametros.put("nomeUf", "%sa%");
+			JasperPrint print = JasperFillManager.fillReport(url.getPath(), parametros, ConexaoUtil.getCon());
+			// JasperPrint print =
+			// JasperFillManager.fillReport("c:/RelatorioUF.jasper", parametros,
+			// ConexaoUtil.getCon());
 
 			JasperViewer.viewReport(print);
 			JasperExportManager.exportReportToPdfFile(print, "relatorio.pdf");
