fd33f2da35e1e2cb290f32ab4cc5d1b3708f1262
João Victor Scanagatta <joaoscanagatta1@hotmail.com>
2017-11-12 21:47:15 -0200

    nda

--- RevisaoOO/src/br/edu/unoesc/revisaoOO/modelo/Conta.java
+++ RevisaoOO/src/br/edu/unoesc/revisaoOO/modelo/Conta.java
@@ -6,20 +6,21 @@ import javafx.scene.control.Alert;
 
 public class Conta implements Serializable {
 
-
-	//private static final long serialVersionUID = -681462113597700012L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// private static final long serialVersionUID = -681462113597700012L;
 	private String numero;
 	private Double saldo;
 	private Double valor;
 	private Long codigo;
-	
-	private Cliente cliente;
-	
-	private Agencia agenciaPreferencial;
-	
-	private Cliente clientePreferencial;
 
-	
+	private Cliente cliente;
+
+	private Agencia agenciaPreferencial;
+
+	private Cliente clientePreferencial;
 
 	public Conta(String numero, Double saldo, Cliente cliente) {
 		super();
@@ -52,10 +53,10 @@ public class Conta implements Serializable {
 	public void depositar(Double valor) {
 		this.saldo += valor;
 
-        Alert dialogoOK1 = new Alert(Alert.AlertType.CONFIRMATION);
-        dialogoOK1.setTitle("SUCESSO");
-        dialogoOK1.setHeaderText("DEPOSITO REALIZADO COM �XITO");
-        dialogoOK1.showAndWait();
+		Alert dialogoOK1 = new Alert(Alert.AlertType.CONFIRMATION);
+		dialogoOK1.setTitle("SUCESSO");
+		dialogoOK1.setHeaderText("DEPOSITO REALIZADO COM �XITO");
+		dialogoOK1.showAndWait();
 	}
 
 	/**
@@ -65,30 +66,28 @@ public class Conta implements Serializable {
 	 */
 	public boolean sacar(Double valor) {
 		if (this.saldo >= valor) {
-			this.setSaldo(this.getSaldo()-valor);
-			
-            Alert dialogoOK = new Alert(Alert.AlertType.CONFIRMATION);
-            dialogoOK.setTitle("SUCESSO");
-            dialogoOK.setHeaderText("SAQUE REALIZADO COM �XITO");
-            dialogoOK.showAndWait();
+			this.setSaldo(this.getSaldo() - valor);
+
+			Alert dialogoOK = new Alert(Alert.AlertType.CONFIRMATION);
+			dialogoOK.setTitle("SUCESSO");
+			dialogoOK.setHeaderText("SAQUE REALIZADO COM �XITO");
+			dialogoOK.showAndWait();
 			return true;
-		}else{
-            Alert dialogoError = new Alert(Alert.AlertType.ERROR);
-            dialogoError.setTitle("ERRO");
-            dialogoError.setHeaderText("A��O N�O PERMITIDA");
-            dialogoError.setContentText("Saldo insuficiente");
-            dialogoError.showAndWait();
+		} else {
+			Alert dialogoError = new Alert(Alert.AlertType.ERROR);
+			dialogoError.setTitle("ERRO");
+			dialogoError.setHeaderText("A��O N�O PERMITIDA");
+			dialogoError.setContentText("Saldo insuficiente");
+			dialogoError.showAndWait();
 			return false;
 		}
 	}
-	
+
 	public void transferir(Conta destino, Double valor) {
 
-		this.setSaldo(this.getSaldo()-valor);
-		destino.setSaldo(destino.getSaldo()+valor);
+		this.setSaldo(this.getSaldo() - valor);
+		destino.setSaldo(destino.getSaldo() + valor);
 	}
-	
-
 
 	// gets e sets
 	public String getNumero() {
@@ -98,8 +97,6 @@ public class Conta implements Serializable {
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-
-	
 
 	public Cliente getCliente() {
 		return cliente;
@@ -112,8 +109,6 @@ public class Conta implements Serializable {
 	public Double getSaldo() {
 		return saldo;
 	}
-	
-	
 
 	public Double getValor() {
 		return valor;
@@ -121,7 +116,7 @@ public class Conta implements Serializable {
 
 	public void setValor(double valor) {
 		this.valor = valor;
-		
+
 	}
 
 	public void setSaldo(Double saldo) {
@@ -129,16 +124,13 @@ public class Conta implements Serializable {
 	}
 
 	@Override
-	public String toString(){
-		return this.cliente.getNome()+ " "+this.numero;
+	public String toString() {
+		return this.cliente.getNome() + " " + this.numero;
 	}
-	
-
 
 	public Agencia getAgenciaPreferencial() {
 		return agenciaPreferencial;
 	}
-
 
 	public void setAgenciaPreferencial(Agencia agenciaPreferencial) {
 		this.agenciaPreferencial = agenciaPreferencial;
@@ -159,7 +151,5 @@ public class Conta implements Serializable {
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-
-
 
 }
